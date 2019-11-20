b124046be1194e97aa4e762e4b94110c0d32af1d
RomainFaure12 <romain.faure@iut.local>
2019-11-20 09:56:47 +0100

    fix #3.2 Moteur de recherche sur status et username

--- all_users.php
+++ all_users.php
@@ -27,31 +27,37 @@
 				throw new PDOException($e->getMessage(),(int)$e->getCode());
 			}
 
-			$lettre = '%';
-			$status_id = '%';
+		?>
 
-			if(isset($_POST['lettre'])){
-				$lettre = $_POST['lettre'];
-			}
+		<form action="all_users.php" method="post">
+			<input id="lettre" name="lettre" type="texte"></input>
+			<select id="status_id" name="status_id">
+			<option value="2">Active account</option>
+			<option value="1">Waiting for account validation</option></select>
+			<input type="submit" name="validation" value="rechercher"/>
+		</form>
+
+		<?php
+
 			if(isset($_POST['status_id'])){
 				$status_id = $_POST['status_id'];
-			}
+				$lettre = $_POST['lettre'];
 
-			echo "<form method=\"post\">";
-			echo "<input id=\"lettre\" type=\"texte\"></input>";
-			echo "<select id=\"status_id\">";
-			echo "<option value=\"1\">Active account</option>";
-			echo "<option value=\"2\">Waiting for account validation</option></select>";
-			echo "<input type=\"submit\" name=\"validation\" value=\"rechercher\"/>";
-			echo "</form>";
-
-			$stmt = $pdo->query("SELECT users.id,username,email,name 
+				$stmt = $pdo->query("SELECT users.id,username,email,name 
 								 FROM users 
 								 JOIN status 
 								 ON users.status_id = status.id 
-								 AND status_id LIKE '$status_id'
+								 AND status_id = $status_id
 								 AND username LIKE '$lettre%' 
 								 ORDER BY username");
+
+			}else{
+				$stmt = $pdo->query("SELECT users.id,username,email,name 
+								 FROM users 
+								 JOIN status 
+								 ON users.status_id = status.id 
+								 ORDER BY username");
+			}
 			echo "<table border=\"1px\">";
 			echo "<tr><td>id</td><td>username</td><td>email</td><td>name</td></tr>";
 			while($row = $stmt->fetch()){
